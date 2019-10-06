d702bc38573830c9c2612d0a7c62b5cb7edf6d3f
Gbenga Omotayo <Gbenga.Omotayo@infor.com>
2019-10-06 01:10:48 -0500

    formatted

--- OnBoarding/src/com/Onboarding/SignUpPatientCommonFunctions.java
+++ OnBoarding/src/com/Onboarding/SignUpPatientCommonFunctions.java
@@ -7,63 +7,59 @@ import java.security.SecureRandom;
 import java.util.Properties;
 
 public class SignUpPatientCommonFunctions {
-	
-    private static final String DATA_FOR_RANDOM_STRING = "abcdefghijklmnopqrstuvwxyz";
-    private static SecureRandom random = new SecureRandom();
-    static Properties prop = new Properties();
-    public static final String Path_Properties = "OnboardingData.properties";
-    
-    
-    /*
-     * This function generates random string. 
-     */
 
-    public static String generateEmail(int length) {
-        if (length < 1) throw new IllegalArgumentException();
+	private static final String DATA_FOR_RANDOM_STRING = "abcdefghijklmnopqrstuvwxyz";
+	private static SecureRandom random = new SecureRandom();
+	static Properties prop = new Properties();
+	public static final String Path_Properties = "OnboardingData.properties";
 
-        StringBuilder sb = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
+	/*
+	 * This function generates random string.
+	 */
+
+	public static String generateEmail(int length) {
+		if (length < 1)
+			throw new IllegalArgumentException();
+
+		StringBuilder sb = new StringBuilder(length);
+		for (int i = 0; i < length; i++) {
 
 			// 0-62 (exclusive), random returns 0-61
-            int rndCharAt = random.nextInt(DATA_FOR_RANDOM_STRING.length());
-            char rndChar = DATA_FOR_RANDOM_STRING.charAt(rndCharAt);
+			int rndCharAt = random.nextInt(DATA_FOR_RANDOM_STRING.length());
+			char rndChar = DATA_FOR_RANDOM_STRING.charAt(rndCharAt);
 
-            sb.append(rndChar);
+			sb.append(rndChar);
 
-        }
+		}
 
-        return sb.toString();
+		return sb.toString();
 
-    }
-    
-    //load data from properties file
-    public static void dataLoad() throws FileNotFoundException, IOException
-    {
-    	prop.load(new FileInputStream(Path_Properties));
-    }
-    
-    //get url from properties file
-    public static String urL()
-    {
-    	String baseUrl = prop.getProperty("roUrl");
+	}
+
+	// load data from properties file
+	public static void dataLoad() throws FileNotFoundException, IOException {
+		prop.load(new FileInputStream(Path_Properties));
+	}
+
+	// get url from properties file
+	public static String urL() {
+		String baseUrl = prop.getProperty("roUrl");
 		return baseUrl;
-    	
-    }
-    
-    //gets firstName from properties file.
-    public static String profileFirstName()
-    {
-    	String firstName = prop.getProperty("firstname");
+
+	}
+
+	// gets firstName from properties file.
+	public static String profileFirstName() {
+		String firstName = prop.getProperty("firstname");
 		return firstName;
-    	
-    }
-    
-    //get lastname from properties file.
-    public static String profileLastName()
-    {
-    	String lastName = prop.getProperty("lastName");
+
+	}
+
+	// get lastname from properties file.
+	public static String profileLastName() {
+		String lastName = prop.getProperty("lastName");
 		return lastName;
-    	
-    }
+
+	}
 
 }
